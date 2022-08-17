f6b638dd225f52cc9f3d0206b754da7aee115788
Abhi Nema <abhinema3@gmail.com>
2022-08-18 00:37:09 +0530

    added .env

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 const connectToDB = require("./db")
 const path = require("path")
 connectToDB();
+require("dotenv").config();
 
 const app = express();
-const port = 5000
+const port = process.env.PORT
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
