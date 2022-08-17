91fb9f2aba8b29996ccb4fc001d16b2e7a359fe4
Abhi Nema <abhinema3@gmail.com>
2022-08-18 00:04:53 +0530

    corrected url atlas

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const url =
-  "mongodb+srv://FETN_backend:cJWPlYG87yNos0Io@cluster0.rjofmvc.mongodb.net/test";
+  "mongodb+srv://FETN_backend:cJWPlYG87yNos0Io@cluster0.rjofmvc.mongodb.net/FETN";
 
 const connectionParams={
 useNewUrlParser: true,
@@ -13,7 +13,7 @@ mongoose.connect(url,connectionParams)
         console.log('Connected to the database ')
     })
     .catch( (err) => {
-        console.error(`Error connecting to the database. n${err}`);
+        console.error(`Error connecting to the database. ${err}`);
     })
 }
 
