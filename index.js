402237cac98939a9f0f869a878b437ac559fe817
Abhi Nema <abhinema3@gmail.com>
2022-12-28 19:59:59 +0530

    multiple hosts

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // app.use(cors())
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://fetn.live", 'https://www.fetn.live'] }));
 // app.use(cors({ credentials: true }));
 
 const cookieParser = require("cookie-parser");
