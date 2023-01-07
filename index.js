2063891a18d2020a7f87a23f63cb9febc93974b5
root <root@projects.lsu1e0qcg20ufacnohwuogr0nc.rx.internal.cloudapp.net>
2023-01-07 18:21:07 +0000

    default value for port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connectToDB();
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
